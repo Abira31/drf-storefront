399804e1e99c7443eef3330d1d0118c89626c641
arturokb2 <tyktybaev_ad@okb2-tmn.ru>
2023-04-05 09:55:21 +0500

    Добавление в store фильтр и пагинаци.

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -3,18 +3,28 @@ from rest_framework import status
 from rest_framework.generics import (ListCreateAPIView,
                                     RetrieveUpdateDestroyAPIView)
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter,OrderingFilter
+from rest_framework.pagination import PageNumberPagination
+
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import (ProductSerializer,
                         CollectionSerializer,
                         ReviewSerializer)
 
 from .models import Product,Collection,Review
-
+from .filters import ProductFilter
+from .pagination import DefaultPagination
 
 class ProducViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.only('id','title','description','slug','inventory','unit_price','collection').all()\
-        .prefetch_related('collection')   
+        .prefetch_related('collection')
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
+    filterset_class = ProductFilter
+    pagination_class = DefaultPagination
+    search_fields = ['title','description']
+    ordering_fields = ['unit_price','last_updated']
 
 class CollectionViewSet(viewsets.ModelViewSet):
     serializer_class = CollectionSerializer
